60a7d2069da23aad9c0693aacf1a306c1667610f
sagepaden <sagepaden@gmail.com>
2023-10-04 09:44:18 -0700

    basica styling with tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Pages/Home'
-import Header from './Components/Header'
+import React, { useState, useContext } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Home from './Pages/Home';
+import Header from './Components/Header';
+import { ThemeContext } from './Context/ThemeContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState('dark');
 
   return (
-    <div className='App'>
-      <Header />
-      <Home />
-    </div>
-    
-  )
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212]' : null}`}>
+        <Header />
+        <Home />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+export default App;
