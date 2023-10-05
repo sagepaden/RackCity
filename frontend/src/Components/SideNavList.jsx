ea77bc29fa1be7efce4834c0b1e2daf479b29c47
sagepaden <sagepaden@gmail.com>
2023-10-04 23:50:14 -0700

    moved pooltable listing to side nav bar.

---
+++ frontend/src/Components/SideNavList.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { getPoolTables } from '../Services/GlobalApi';
+
+function SideNavList() {
+  const [poolTableList, setPoolTableList] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const getPoolTablesData = async () => {
+    try {
+      setLoading(true); // Set loading to true while fetching data
+      const response = await getPoolTables(); // Use the Axios API function from GlobalApi
+      const poolTableData = response.data;
+      setPoolTableList(poolTableData); // Update the state with fetched data
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false); // Set loading back to false after fetching data
+    }
+  };
+
+  useEffect(() => {
+    getPoolTablesData(); // Fetch data when the component mounts
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className=''>
+          <h3 className='font-bold text-[30px] dark:text-white '>Genres</h3>
+          {poolTableList.map((item, index) => (
+            <div
+              key={index}
+              className={`flex gap-2 items-center cursor-pointer 
+          group  transition-all duration-300 rounded-lg p-3 
+          ${activeIndex == index ? 'bg-slate-300 dark:bg-gray-700 ' : null}`}
+              onClick={() => {
+                setActiveIndex(index), selectedGenreId(item.id);
+              }}
+            >
+              <img
+                src={item.image_background}
+                className={`w-[40px] h-[40px] 
+                object-cover rounded-lg group-hover:scale-110  
+                transition-all duration-300 ${
+                  activeIndex == index ? 'scale-110' : null
+                } `}
+              />
+              <h3
+                className={`text-[18px] group-hover:font-bold   dark:text-white
+                transition-all duration-300 ${
+                  activeIndex == index ? 'font-bold' : null
+                }`}
+              >
+                {item.location_name}
+              </h3>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SideNavList;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,16 +2,20 @@ import React, { useEffect, useState } from 'react';
 import MapComponent from '../Components/MapComponent';
 import AllPoolTableList from '../Components/AllPoolTableList';
 import PopularPoolTableList from '../Components/PopularPoolTableList';
+import SideNavList from '../Components/SideNavList';
 // import SideNavGenreList from '../Componets/SideNavPoolList';
 // import GlobalApi from '../Services.js/GlobalApi';
 
-
 function Home() {
   return (
-    <div>
-      <MapComponent />
-
-   <PopularPoolTableList />
+    <div className='grid grid-cols-4 p-8'>
+      <div className='hidden md:flex'>
+        <SideNavList />
+      </div>
+      <div className='md:col-span-3 col-span-4 px-3'>
+        <MapComponent />
+        <PopularPoolTableList />
+      </div>
     </div>
   );
 }
