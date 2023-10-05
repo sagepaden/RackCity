2001b5e910647edd0f28d8ba34696e57c2e2812b
sagepaden <sagepaden@gmail.com>
2023-10-04 21:26:42 -0700

    deleted unneeded files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import PoolTableList from './Components/PoolTableList';
-import { ThemeContext } from './Context/ThemeProvider';
 import Header from './Components/Header';
-import MapComponent from './Components/MapComponent';
 import Home from '../../frontend/src/Pages/Home';
 import { ThemeProvider } from './Context/ThemeProvider';
 
