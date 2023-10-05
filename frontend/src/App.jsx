4a3c6e0f9c27ff4ca623f1bf8f50a04e2e99381c
sagepaden <sagepaden@gmail.com>
2023-10-04 19:20:43 -0700

    Header and light and dark mode is set up and functioning properly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PoolTableList from './Components/PoolTableList';
+import { ThemeContext } from './Context/ThemeContext';
+import Header from './Components/Header';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  useEffect(() => {
+    setTheme(
+      localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark'
+    );
+  }, []);
 
   return (
-    <PoolTableList />
-  )
-  
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`${theme} ${theme == 'dark' ? 'bg-[#121212]' : null}`}>
+        <Header />
+        <PoolTableList />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
